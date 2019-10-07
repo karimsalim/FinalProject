a8b7f4d53470d2ee1bc34f2b43f6421b33814bdb
Ghassan <ghassan2hamdan@gmail.com>
2019-10-07 15:26:41 +0200

    Class Client

---
+++ DAL/Client.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class Client : Person
+
+    {       /*les propriétés clients */
+        [Required(ErrorMessage ="Veuillez saisir la rue ")]
+        public string Street { get; set; }
+
+        [Required(ErrorMessage ="Veuillez saisir votre ZideCode")]
+        public string ZideCode { get; set; }
+          
+        [Required(ErrorMessage ="Veuillez saisir la Ville ")]
+        public string City { get; set; }
+       
+        //propriétés commun avec class person 
+        public virtual List<Account> Accounts { get; set; }
+        public virtual Person Person { get; set; }
+        
+    }
+
+}
