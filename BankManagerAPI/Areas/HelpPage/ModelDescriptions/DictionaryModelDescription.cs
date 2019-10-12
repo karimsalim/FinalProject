9e8b7cb7314364f01628a6621bcf172c2fc85baf
Hiirome <gael.coudurier-curveur@ynov.com>
2019-10-12 18:50:39 +0200

    Generation of WPF and WebAPI projects

---
+++ BankManagerAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace BankManagerAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ BankManagerAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace BankManagerAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
