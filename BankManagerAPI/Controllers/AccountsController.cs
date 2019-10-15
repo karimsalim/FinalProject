cec30cddbe860a15a9a80ecdd273eac693a2b6f8
Hiirome <gael.coudurier-curveur@ynov.com>
2019-10-15 17:29:38 +0200

    Dirty Manager Cntroller that Works, Account Controller that needs some adjustements.

---
+++ BankManagerAPI/Controllers/AccountsController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using DAL;
+
+namespace BankManagerAPI.Controllers
+{
+    public class AccountsController : ApiController
+    {
+        private BankContext db = new BankContext();
+
+        // GET: api/Accounts
+        public decimal GetTotalBalance()
+        {
+            return db.Accounts.Sum(e => e.Balance);
+        }
+
+        // GET: api/Accounts/5
+        //[ResponseType(typeof(Account))]
+        //public async Task<IHttpActionResult> GetAccount(int id)
+        //{
+        //    Account account = await db.Accounts.FindAsync(id);
+        //    if (account == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    return Ok(account);
+        //}
+
+        public decimal GetSavingSum()
+        {
+            return db.Savings.Sum(e => e.Balance);
+        }
+
+        public decimal GetSavingSumManaged(int personId)
+        {
+            return db.Savings.Include("Person").Where(e => e.Client.Conseiller.Manager.PersonId == personId).Sum(e => e.Balance);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool AccountExists(int id)
+        {
+            return db.Accounts.Count(e => e.AccountID == id) > 0;
+        }
+    }
+}
