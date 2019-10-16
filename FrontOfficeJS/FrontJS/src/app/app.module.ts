b7c24c635f01255619ad2bfb9d2afb2bc4400a48
Karim Salim <karimsalimmaadoul@gmail.com>
2019-10-16 23:37:10 +0200

    Ajout Layout-c & Home-c + FlexLayout

--- FrontOfficeJS/FrontJS/src/app/app.module.ts
+++ FrontOfficeJS/FrontJS/src/app/app.module.ts
@@ -7,18 +7,25 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MyMaterialModule} from './material.module';
 import { LoginClientComponent } from './login-client/login-client.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { LayoutComponent } from './Template/layout/layout.component';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import { HomeComponent } from './Template/home/home.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginClientComponent
+    LoginClientComponent,
+    LayoutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MyMaterialModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
