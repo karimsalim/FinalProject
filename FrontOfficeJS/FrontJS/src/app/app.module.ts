ec0a279cd6847a503d7fb201a8a1842f28069525
Karim Salim <karimsalimmaadoul@gmail.com>
2019-10-16 22:41:15 +0200

    Creation page connexion + import material

--- FrontOfficeJS/FrontJS/src/app/app.module.ts
+++ FrontOfficeJS/FrontJS/src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import 'hammerjs';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MyMaterialModule} from './material.module';
+import { LoginClientComponent } from './login-client/login-client.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginClientComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MyMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
