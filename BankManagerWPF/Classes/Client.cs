6bd9411f3ad21aa54e51d51b6f06d563f7da1ba4
Hiirome <gael.coudurier-curveur@ynov.com>
2019-10-22 10:16:59 +0200

    Second Cosmetic Pass

--- BankManagerWPF/Classes/Client.cs
+++ BankManagerWPF/Classes/Client.cs
@@ -9,12 +9,12 @@ namespace BankManagerWPF.Classes
 {
     class Client
     {
-        public string Street { get; set; }
-        public string ZipCode { get; set; }
-        public string City { get; set; }
         public int PersonId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string Street { get; set; }
+        public string ZipCode { get; set; }
+        public string City { get; set; }        
         public DateTime DateOfBirth { get; set; }
     }
 }
